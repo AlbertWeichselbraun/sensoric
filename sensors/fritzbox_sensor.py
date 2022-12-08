af1fa6afbba431ba26e93037ea2478cd083ba7bd
Albert Weichselbraun <albert@weichselbraun.net>
2022-12-08 11:50:39 +0100

    chg: improved fritzbox module :)

--- sensors/fritzbox_sensor.py
+++ sensors/fritzbox_sensor.py
@@ -1,32 +1,28 @@
 #!/usr/bin/env python3
 """
-Fritz!Box sensor based on the TR-064 protocol.
+Fritz!Box sensor based on fritzconnection
 
-Examples:
-  https://znil.net/index.php?title=FritzBox_Status_Informationen_per_Upnp_mit_Linux_Bash-Script_auslesen
+https://fritzconnection.readthedocs.io
 """
 
 from os import getenv
-from http.client import HTTPConnection
-from xml.etree import ElementTree
+from fritzconnection.lib.fritzstatus import FritzStatus
 
 SKIP = 1
 FRITZ_HOST = getenv('SENSORIC_FRITZ_HOST')
-FRITZ_PORT = int(getenv('SENSORIC_FRITZ_PORT') or 49000)
+FRITZ_PORT = int(getenv('SENSORIC_FRITZ_PORT', '49000'))
 if not FRITZ_HOST or not FRITZ_PORT:
     print('SENSORIC_FRITZ_HOST and/or SENSORIC_FRITZ_PORT environment variable not set.')
     exit(-1)
 
-COUNTERS = {'NewTotalBytesSent': 'bytes_sent',
-            'NewTotalBytesReceived': 'bytes_recv'}
+COUNTERS = {'bytes_sent': 'bytes_sent',
+            'bytes_received': 'bytes_recv',
+            'is_connected': 'is_connected',
+            'transmission_rate': 'stream',
+            'max_byte_rate': 'stream_max'}
 
-REQ = """<?xml version="1.0" encoding="utf-8" ?>
-         <s:Envelope s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
-           <s:Body>
-              <u:GetCommonLinkProperties xmlns:u="urn:schemas-upnp-org:service:WANCommonInterfaceConfig:1" />
-           </s:Body>
-         </s:Envelope>"""
-XPATH = ".//{urn:schemas-upnp-org:service:WANCommonInterfaceConfig:1}GetAddonInfosResponse/"
+fs = FritzStatus(address=FRITZ_HOST, port=FRITZ_PORT, use_tls=False,
+                 use_cache=True, verify_cache=True)
 
 
 def setup():
@@ -42,44 +38,15 @@ def get_sensor_tags():
 
 
 def get_sensor_fields():
-    c = HTTPConnection(FRITZ_HOST, FRITZ_PORT)
-    c.request('PUT', url='/igdupnp/control/WANPPPConnection1', body=REQ.replace("WANCommonInterfaceConfig",
-                                                                                "WANPPPConnection"),
-              headers={'Content-Type': 'text/xml; charset="utf-8"',
-                       'SoapAction': 'urn:schemas-upnp-org:service:WANPPPConnection:1'})
-    response = c.getresponse()
-    if response.status == 200:
-        pass
-    else:
-        print(f'WARNING: Cannot obtain statistics from '
-              f'{FRITZ_HOST}:{FRITZ_PORT} '
-              f'{response.reason} ({response.status}).')
-
     result = {}
-    data = response.read()
-
-    return result
-
-    c = HTTPConnection(FRITZ_HOST, FRITZ_PORT)
-    c.request('PUT', url='/igdupnp/control/WANCommonIFC1', body=REQ,
-              headers={'Content-Type': 'text/xml; charset="utf-8"',
-                       'SoapAction': 'urn:schemas-upnp-org:service:WANCommonInterfaceConfig:1#GetAddonInfos'})
-    response = c.getresponse()
-    if response.status == 200:
-        pass
-    else:
-        print(f'WARNING: Cannot obtain statistics from '
-              f'{FRITZ_HOST}:{FRITZ_PORT} '
-              f'{response.reason} ({response.status}).')
-
-    result = {}
-    data = response.read()
-    tree = ElementTree.fromstring(data.decode('utf-8'))
-    for element in tree.findall(XPATH):
-        print(element, element.text)
-        if element.tag in COUNTERS:
-            key = 'fritz_' + COUNTERS[element.tag]
-            result[key] = int(element.text)
+    for soap_call, key in COUNTERS.items():
+        value = getattr(fs, soap_call)
+        if isinstance(value, tuple):
+            up, down = value
+            result[f'fritz_up{key}'] = int(up)
+            result[f'fritz_down{key}'] = int(down)
+        else:
+            result[f'fritz_{key}'] = int(value)
 
     return result
 
